833e2d81f874558a70708c30e4006f090ad2557d
kakioshe <louis.kakioshe@gmail.com>
2019-10-06 00:38:32 +0700

    refactor code

--- zip_cracker/unzip.py
+++ zip_cracker/unzip.py
@@ -8,30 +8,24 @@ from threading import Thread
 def tryPass(zFile, password):
     try:
       zFile.extractall(pwd=bytes(password, 'utf-8'))
-      print('[+] Password = ' + password + '\n')
-    except Exception as e:
+      print('Found Password = ' + password + '\n')
+    except Exception:
       pass
+
 
 def argCheck():
   if len(sys.argv) == 3:
-    filename = sys.argv[1]
-    dictFile = sys.argv[2]
-    if not os.path.isfile(filename):
-        print(filename + " Does not Exist")
-        exit(0)
-    if not os.path.isfile(dictFile):
-        print(dictFile + " Does not Exist")
-        exit(0)
-    if not os.access(filename, os.R_OK):
-        print(filename + "Access Denied")
-        exit(0)
-    if not os.access(dictFile, os.R_OK):
-        print(dictFile + "Access Denied")
-        exit(0)
+    files = sys.argv[1:]
+    for file in files:
+        if not os.path.isfile(file):
+            print(file) + " Does not exist"
+            exit(0)
+        if not os.access(file, os.R_OK):
+            print(file + " Acess Denied")
+            exit(0)
   else:
-    print("Specify a .zip file and dict list")
+    print(".zip File and Dictionary List are required. Use -h for help")
     exit(0)
-
 
 def main():
   argCheck()
@@ -45,7 +39,8 @@ def main():
 
 def parse_arguments():
   parser=argparse.ArgumentParser(
-    description='''Basic Zip File Password Cracker ''')
+    description='''Basic Zip File Password Cracker ''',
+    epilog='-- Created by N4L.A')
   parser.add_argument('zipFile', nargs='+', help='ZIP file')
   parser.add_argument('dictFile', nargs='+', help='Dictionary list file')
   args=parser.parse_args()
